0ec800837c724b2f7f3b8c2af0af9d4c7db4d562
Till Theis <theis.till@gmail.com>
2013-03-10 23:53:30 +0100

    extract clocks controller

--- fullscreenclock/AppDelegate.rb
+++ fullscreenclock/AppDelegate.rb
@@ -4,116 +4,87 @@ require 'time'
 
 require 'exts'
 
+require 'ClocksController'
+
 
 class AppDelegate
     attr_accessor :background_alpha_slider, :face_alpha_slider, :hands_alpha_slider
-    attr_accessor :window, :minute_timer, :clock_windows
-    attr_accessor :fading_timer, :fading_step, :fading_interval, :current_alpha
-    attr_accessor :background_alpha, :hands_alpha, :face_alpha
-    attr_accessor :defaults, :visible
+    attr_accessor :window
+    attr_accessor :defaults
     attr_accessor :status_item, :status_item_menu, :toggle_clocks_menu_item
-    
-    alias_method :visible?, :visible
-    
+
+    attr_accessor :clocks_controller
+
     include CocoaCompatibility
-    
-        
+
+
     def applicationDidFinishLaunching(a_notification)
-        self.clock_windows = [];
+        init_defaults
 
-        self.fading_step     = 0.02
-        self.fading_interval = 0.05
-        self.current_alpha   = 0.0
+        self.clocks_controller = ClocksController.new(NSScreen.screens.drop(1))
 
-        self.visible = false
+        ["background_alpha", "face_alpha", "hands_alpha"].each do |attribute|
+            path = "values." + attribute
+            clocks_controller.bind(attribute, toObject:NSUserDefaultsController.sharedUserDefaultsController, withKeyPath:path, options:nil)
+        end
+        
+        clocks_controller.addObserver(self, forKeyPath:"visible", options:NSKeyValueObservingOptionNew, context:nil)
 
-        FullscreenObserver.sharedFullscreenObserver.addObserver(self, forKeyPath:"fullscreenMode", options:NSKeyValueObservingOptionNew, context:nil)
 
         NSWorkspace.sharedWorkspace.notificationCenter.addObserver(self, selector:'workspaceChanged:', name:NSWorkspaceActiveSpaceDidChangeNotification, object:nil)
-        
-        
-        # set alpha values
-        init_defaults
-        
-        
+
+
         window.releasedWhenClosed = false
-        
+
         # only show when explicitly requested
         # applicationShouldHandleReopen:hasVisibleWindows: handles that
         window.close
-        
+
         # show above clock
         window.level = NSFloatingWindowLevel
-        
+
         show_menu_bar_icon if defaults.boolForKey("show_menu_bar_icon")
     end
     
+    
     def observeValueForKeyPath(keyPath, ofObject:object, change:change, context:context)
-        # keyPath is "fullscreenMode" of object FullscreenObserver.sharedFullscreenObserver
-        if change[NSKeyValueChangeNewKey].boolValue
-            setup_clocks
-        else
-            teardown_clocks
+        if object == clocks_controller && keyPath == "visible"
+            is_visible = change[NSKeyValueChangeNewKey]
+            title = if is_visible then "Hide Clocks" else "Show Clocks" end
+            self.toggle_clocks_menu_item.title = NSLocalizedString(title)
+        
+            # float command window above the clock windows
+            window.orderFront(self) if window.isKeyWindow
         end
     end
 
     def workspaceChanged(trigger)
-        clean_up
+        clocks_controller.hide_immediately
     end
-    
+
     def applicationShouldHandleReopen(app, hasVisibleWindows:flag)
         window.makeKeyAndOrderFront(self)
     end
-    
-    def applicationDidChangeScreenParameters(notification)
-        # remove clocks from removed screens and (maybe new) primary screen
-        allowed_screens = NSScreen.screens.drop(1)
-        
-        old_clock_windows = clock_windows
-        self.clock_windows = []
 
-        old_clock_windows.each do |w|
-            if !allowed_screens.include?(w.screen)
-                w.delegate = nil
-                w.releasedWhenClosed = true
-                w.close
-            else
-                clock_windows << w
-            end
-        end
-        
-        
-        # add clocks to new screens
-        if FullscreenObserver.sharedFullscreenObserver.isFullscreenMode
-            t = Time.now
-            used_screens = clock_windows.map &:screen
-            unused_screens = allowed_screens - used_screens
-            unused_screens.each do |scr|
-                w = setup_screen(scr, t)
-                w.alphaValue = 1.0 # no fading
-            end
-        end
-        
-        
-        # stop timer etc. if no clocks are left
-        teardown_clocks if clock_windows.empty?
+    def applicationDidChangeScreenParameters(notification)
+        clocks_controller.screens = NSScreen.screens.drop(1)
     end
-    
+
     def showAboutPanel(sender)
         NSApp.orderFrontStandardAboutPanel(sender)
         NSApp.activateIgnoringOtherApps(true) # or won't show if called via status bar menu
     end
-    
+
     def showPreferencesWindow(sender)
         window.makeKeyAndOrderFront(sender)
         NSApp.activateIgnoringOtherApps(true) # or won't be focused if called via status bar menu
     end
-    
+
     def changeMenuBarIconVisibility(sender)
         if sender.state == 0
             hide_menu_bar_icon
-            
-            
+
+
             title = NSLocalizedString("Info")
             message = NSLocalizedString("You can still go to this preferences window by double-clicking the application icon.")
 
@@ -122,14 +93,22 @@ class AppDelegate
             show_menu_bar_icon
         end
     end
-    
-    
-    
+
+    def toggle_clocks(sender)
+        if clocks_controller.visible?
+            clocks_controller.hide
+        else
+            clocks_controller.show
+        end
+    end
+
+
+
     #################
     # MENU BAR ICON #
     #################
-    
-    
+
+
     def show_menu_bar_icon
         self.status_item = NSStatusBar.systemStatusBar.statusItemWithLength(NSSquareStatusItemLength)
         self.status_item.menu = status_item_menu
@@ -137,256 +116,39 @@ class AppDelegate
         self.status_item.toolTip = NSRunningApplication.currentApplication.localizedName
         self.status_item.image = ClockView.alloc.initWithFrame([[0, 0], [16, 16]], time:Time.parse("06:50")).to_image
     end
-    
+
     def hide_menu_bar_icon
         NSStatusBar.systemStatusBar.removeStatusItem(status_item)
         self.status_item = nil
     end
-    
-    
-    
+
+
+
     ############
     # DEFAULTS #
     ############
-    
-    
+
+
     private
-    
-    
+
+
     def default_values()
         path = NSBundle.mainBundle.pathForResource("UserDefaults", ofType:"plist")
-        dict = NSDictionary.dictionaryWithContentsOfFile(path);
+        dict = NSDictionary.dictionaryWithContentsOfFile(path)
     end
-    
+
     def init_defaults()
         self.defaults = NSUserDefaults.standardUserDefaults
-        
+
         dict = default_values
         defaults.registerDefaults(dict)
-        
-        self.background_alpha = defaults.floatForKey('background_alpha')
-        self.face_alpha       = defaults.floatForKey('face_alpha')
-        self.hands_alpha      = defaults.floatForKey('hands_alpha')
     end
-    
+
     def restore_defaults(sender)
         dict = default_values
-        
-        self.background_alpha = dict['background_alpha']
-        self.face_alpha       = dict['face_alpha']
-        self.hands_alpha      = dict['hands_alpha']
-    end
-    
-    
-    
-    ####################
-    # CLOCK MANAGEMENT #
-    ####################
-    
-    
-    private
-    
-        
-    def toggle_clocks(sender)
-        if !visible?
-            setup_clocks
-        else
-            teardown_clocks
-        end
-    end
-    
-    def setup_clocks()
-        if clock_windows.empty?
-            t = Time.now + 60 # +1 minute
-            fire_date = Time.mktime(t.year, t.month, t.day, t.hour, t.min)
-            self.minute_timer = NSTimer.alloc.initWithFireDate(fire_date,
-                                                               interval:60,
-                                                               target:self,
-                                                               selector:'update_clock_view:',
-                                                               userInfo:nil,
-                                                               repeats:true)
-            NSRunLoop.currentRunLoop.addTimer(self.minute_timer, forMode:NSDefaultRunLoopMode)
 
-            # draw clocks on secondary screens
-            # screens.first is the the mainScreen - remove it
-            # (we can't use delete() because its destructive)
-            screens = NSScreen.screens.drop(1)
-
-            t = Time.now
-            screens.each { |scr| setup_screen(scr, t) }
-
-            # float command window above the clock windows
-            window.orderFront(self) if window.isKeyWindow
-        else
-            # no need to create new clocks because the old ones are still fading out
-        end
-        
-        self.visible = true
-        fade_in
-    end
-    
-    def update_clock_view(timer)
-        t = Time.now
-        self.clock_windows.each do |win|
-            win.contentView.subviews.first.time = t
-        end
-    end
-    
-    def setup_screen(screen, time)
-        screenBounds = [NSZeroPoint, screen.frame.size]
-        
-        window = NSWindow.alloc.initWithContentRect(screenBounds,
-                                                    styleMask:NSBorderlessWindowMask,
-                                                    backing:NSBackingStoreBuffered,
-                                                    defer:false,
-                                                    screen:screen)     
-        
-        window.opaque          = false
-        window.level           = NSFloatingWindowLevel
-        window.alphaValue      = 0.0    # don't draw immediately
-        window.backgroundColor = NSColor.colorWithCalibratedWhite(0, alpha:background_alpha)
-        window.hasShadow       = false
-        
-        window.delegate = self
-        def window.mouseDown(notification)
-            close
-        end
-        
-        view = ClockView.alloc.initWithFrame(screenBounds, time:time)
-        view.face_alpha  = face_alpha
-        view.hands_alpha = hands_alpha
-        window.contentView.addSubview(view)
-        window.orderFront(self)
-        
-        self.clock_windows << window
-        
-        window
-    end
-    
-    def teardown_clocks()
-        self.visible = false
-        fade_out
-    end
-    
-    def clean_up()
-        minute_timer.invalidate unless minute_timer.nil?
-        fading_timer.invalidate unless fading_timer.nil?
-
-        # close windows here because timer doesn't always take the same time
-        # (when windows were not completely opaque when fading out began)
-        clock_windows.each &:close
-        self.clock_windows = []
-        self.current_alpha = 0.0
-    end
-    
-    def windowWillClose(notification)
-        clock_windows.delete(notification.object)
-        
-        if clock_windows.empty?
-            self.visible = false
-            clean_up
-        end
-    end
-    
-    
-    
-    ##########
-    # FADING #
-    ##########
-    
-    
-    private
-    
-    
-    def fade(fading_selector)
-        fading_timer.invalidate unless fading_timer.nil?
-        
-        self.fading_timer =
-            NSTimer.timerWithTimeInterval(fading_interval,
-                                          target:self,
-                                          selector:fading_selector,
-                                          userInfo:nil,
-                                          repeats:true)
-        
-        # don't use NSDefaultRunloopMode because Mac OS X Lion pauses timers
-        # in that mode whenever the user drags NSSliders around
-        NSRunLoop.currentRunLoop.addTimer(self.fading_timer, forMode:NSRunLoopCommonModes)
-    end
-    
-    
-    def fade_in()
-        fade('fade_in_do:')
-    end
-    
-    def fade_out()
-        fade('fade_out_do:')
-    end
-    
-    
-    def fade_step_do(timer, step)
-        self.current_alpha += step
-        clock_windows.each { |w| w.alphaValue = current_alpha }
-    end
-    
-    
-    def fade_in_do(timer)
-        fade_step_do(timer, +fading_step)
-        timer.invalidate if current_alpha >= 1.0
-    end
-
-    def fade_out_do(timer)
-        fade_step_do(timer, -fading_step)
-        
-        if current_alpha <= 0.0
-            timer.invalidate
-            clean_up
-        end
-    end
-    
-    
-    
-    ##############
-    # PROPERTIES #
-    ##############
-    
-    
-    public
-    
-
-    def background_alpha=(alpha)
-        set_ivar(:background_alpha, alpha) do
-            defaults.setFloat(alpha, forKey:'background_alpha')
-            
-            self.clock_windows.each do |win|
-                win.backgroundColor = NSColor.colorWithCalibratedWhite(0, alpha:alpha)
-            end
-        end
-    end
-    
-    def face_alpha=(alpha)
-        set_ivar(:face_alpha, alpha) do
-            defaults.setFloat(alpha, forKey:'face_alpha')
-            
-            self.clock_windows.each do |win|
-                win.contentView.subviews.first.face_alpha = alpha
-            end
-        end
-    end
-    
-    def hands_alpha=(alpha)
-        set_ivar(:hands_alpha, alpha) do
-            defaults.setFloat(alpha, forKey:'hands_alpha')
-            
-            self.clock_windows.each do |win|
-                win.contentView.subviews.first.hands_alpha = alpha
-            end
-        end
-    end
-    
-    def visible=(is_visible)
-        set_ivar(:visible, is_visible) do
-            title = if is_visible then "Hide Clocks" else "Show Clocks" end
-            self.toggle_clocks_menu_item.title = NSLocalizedString(title)
-        end
+        defaults.setFloat(dict["background_alpha"], forKey:"background_alpha")
+        defaults.setFloat(dict["face_alpha"], forKey:"face_alpha")
+        defaults.setFloat(dict["hands_alpha"], forKey:"hands_alpha")
     end
 end
---
+++ fullscreenclock/ClocksController.rb
@@ -0,0 +1,294 @@
+require 'exts'
+
+class ClocksController
+public
+    include CocoaCompatibility
+
+    attr_reader :visible
+    alias_method :visible?, :visible
+
+    attr_accessor :screens, :background_alpha, :hands_alpha, :face_alpha
+
+
+#private # can't make these private or macruby can't access these anymore if method is called via the ui
+    attr_accessor :windows, :minute_timer, :fullscreen_observer #, :visible must be defined manually - for KVO compliance
+    attr_accessor :fading_timer, :fading_step, :fading_interval, :current_alpha
+
+
+public
+    def initialize(screens)
+        self.windows = []
+
+        self.fading_step     = 0.02
+        self.fading_interval = 0.05
+        self.current_alpha   = 0.0
+
+        self.background_alpha = 0.5
+        self.hands_alpha      = 1.0
+        self.face_alpha       = 1.0
+
+        self.visible = false
+
+        self.fullscreen_observer = FullscreenObserver.sharedFullscreenObserver
+        fullscreen_observer.addObserver(self, forKeyPath:"fullscreenMode", options:NSKeyValueObservingOptionNew, context:nil)
+
+        self.screens = screens
+    end
+
+    def show
+        setup_clocks unless visible?
+    end
+
+    def hide
+        teardown_clocks if visible?
+    end
+
+    def hide_immediately
+        clean_up
+    end
+
+    def screens=(allowed_screens)
+        # remove clocks from removed screens and (maybe new) primary screen
+
+        old_clock_windows = windows
+        self.windows = []
+
+        old_clock_windows.each do |window|
+            puts "old_clock_windows.each"
+            if !allowed_screens.include?(window.screen)
+                window.delegate = nil
+                window.releasedWhenClosed = true
+                window.close
+            else
+                windows << window
+            end
+        end
+
+        # MacRuby returns fullscreen_observer.isFullscreenMode as int instead of bool
+        # therefore we have to check for 0 or 1
+
+        # add clocks to new screens
+        if fullscreen_observer.isFullscreenMode == true || fullscreen_observer.isFullscreenMode == 1
+            now = Time.now
+            used_screens = windows.map(&:screen)
+            unused_screens = allowed_screens - used_screens
+            unused_screens.each do |screen|
+                window = setup_screen(screen, now)
+                window.alphaValue = 1.0 # no fading
+            end
+        end
+
+
+        # stop timer etc. if no clocks are left
+        clean_up if windows.empty?
+    end
+
+    def background_alpha=(alpha)
+        set_ivar(:background_alpha, alpha) do
+            windows.each do |window|
+                window.backgroundColor = NSColor.colorWithCalibratedWhite(0, alpha:alpha)
+            end
+        end
+    end
+
+    def face_alpha=(alpha)
+        set_ivar(:face_alpha, alpha) do
+            windows.each do |window|
+                window.contentView.subviews.first.face_alpha = alpha
+            end
+        end
+    end
+
+    def hands_alpha=(alpha)
+        set_ivar(:hands_alpha, alpha) do
+            windows.each do |window|
+                window.contentView.subviews.first.hands_alpha = alpha
+            end
+        end
+    end
+
+
+public
+    def observeValueForKeyPath(keyPath, ofObject:object, change:change, context:context)
+        if object == fullscreen_observer && keyPath == "fullscreenMode"
+            if change[NSKeyValueChangeNewKey].boolValue
+                show
+            else
+                hide
+            end
+        end
+    end
+
+
+private
+
+
+
+    ####################
+    # CLOCK MANAGEMENT #
+    ####################
+
+
+    def setup_clocks()
+        if windows.empty?
+            t = Time.now + 60 # +1 minute
+            fire_date = Time.mktime(t.year, t.month, t.day, t.hour, t.min)
+            self.minute_timer = NSTimer.alloc.initWithFireDate(fire_date,
+                                                               interval:60,
+                                                               target:self,
+                                                               selector:'update_clock_view:',
+                                                               userInfo:nil,
+                                                               repeats:true)
+            NSRunLoop.currentRunLoop.addTimer(self.minute_timer, forMode:NSDefaultRunLoopMode)
+
+            # draw clocks on secondary screens
+            # screens.first is the the mainScreen - remove it
+            # (we can't use delete() because its destructive)
+            screens = NSScreen.screens.drop(1)
+
+            t = Time.now
+            screens.each { |scr| setup_screen(scr, t) }
+        else
+            # no need to create new clocks because the old ones are still fading out
+        end
+
+        self.visible = true
+        fade_in
+    end
+
+    def update_clock_view(timer)
+        t = Time.now
+        self.windows.each do |win|
+            win.contentView.subviews.first.time = t
+        end
+    end
+
+    def setup_screen(screen, time)
+        screenBounds = [NSZeroPoint, screen.frame.size]
+
+        window = NSWindow.alloc.initWithContentRect(screenBounds,
+                                                    styleMask:NSBorderlessWindowMask,
+                                                    backing:NSBackingStoreBuffered,
+                                                    defer:false,
+                                                    screen:screen)
+
+        window.opaque          = false
+        window.level           = NSFloatingWindowLevel
+        window.alphaValue      = 0.0    # don't draw immediately
+        window.backgroundColor = NSColor.colorWithCalibratedWhite(0, alpha:background_alpha)
+        window.hasShadow       = false
+
+        window.delegate = self
+        def window.mouseDown(notification)
+            close
+        end
+
+        view = ClockView.alloc.initWithFrame(screenBounds, time:time)
+        view.face_alpha  = face_alpha
+        view.hands_alpha = hands_alpha
+        window.contentView.addSubview(view)
+        window.orderFront(self)
+        
+        #["face_alpha", "hands_alpha"].each do |attribute|
+        #    viewProxy.bind(attribute, toObject:self, withKeyPath:attribute, options:nil)
+        #end
+        
+        #window.bind("background_alpha", toObject:self, withKeyPath:"background_alpha", options:nil)
+
+        windows << window
+
+        window
+    end
+
+    def teardown_clocks()
+        self.visible = false
+        fade_out
+    end
+
+    def clean_up()
+        minute_timer.invalidate unless minute_timer.nil?
+        fading_timer.invalidate unless fading_timer.nil?
+
+        # close windows here because timer doesn't always take the same time
+        # (when windows were not completely opaque when fading out began)
+        windows.each &:close
+        self.windows = []
+        self.current_alpha = 0.0
+        self.visible = false
+    end
+
+    def windowWillClose(notification)
+        windows.delete(notification.object)
+
+        if windows.empty?
+            self.visible = false
+            clean_up
+        end
+    end
+
+
+
+    ##########
+    # FADING #
+    ##########
+
+
+    def fade(fading_selector)
+        fading_timer.invalidate unless fading_timer.nil?
+
+        self.fading_timer =
+            NSTimer.timerWithTimeInterval(fading_interval,
+                                          target:self,
+                                          selector:fading_selector,
+                                          userInfo:nil,
+                                          repeats:true)
+
+        # don't use NSDefaultRunloopMode because Mac OS X Lion pauses timers
+        # in that mode whenever the user drags NSSliders around
+        NSRunLoop.currentRunLoop.addTimer(self.fading_timer, forMode:NSRunLoopCommonModes)
+    end
+
+
+    def fade_in()
+        fade('fade_in_do:')
+    end
+
+    def fade_out()
+        fade('fade_out_do:')
+    end
+
+
+    def fade_step_do(timer, step)
+        self.current_alpha += step
+        windows.each { |w| w.alphaValue = current_alpha }
+    end
+
+
+    def fade_in_do(timer)
+        fade_step_do(timer, +fading_step)
+        timer.invalidate if current_alpha >= 1.0
+    end
+
+    def fade_out_do(timer)
+        fade_step_do(timer, -fading_step)
+
+        if current_alpha <= 0.0
+            timer.invalidate
+            clean_up
+        end
+    end
+    
+    
+    
+    ##############
+    # PROPERTIES #
+    ##############
+    
+    
+    # MacRuby doesn't seem to always generate KVO-compliant setters
+    def setVisible(is_visible)
+        set_ivar(:visible, is_visible)
+    end
+    
+    alias_method :"visible=", :setVisible
+end
